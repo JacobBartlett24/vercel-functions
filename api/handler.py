7b49e472ddbdb4954bdb09bc7bdf1d1a00781801
Jacob Bartlett <jacobart@ttu.edu>
2023-03-24 17:51:50 -0500

    remove vercel class

--- api/handler.py
+++ api/handler.py
@@ -130,12 +130,4 @@ def hello():
       
   requestTarget()
 
-class handler(BaseHTTPRequestHandler):    
-  hello()
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type','text/plain')
-    self.end_headers()
-    self.wfile.write('Hello, world!'.encode('utf-8'))
-    return
-
+hello()
